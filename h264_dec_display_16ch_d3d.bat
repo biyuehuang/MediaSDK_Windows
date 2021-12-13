bee9a70fbab67627024a7259dff049e2a0dbc2ea
biyuehuang <31006098+biyuehuang@users.noreply.github.com>
2021-12-13 11:20:21 +0800

    decode with display by video wall

---
+++ h264_dec_display_16ch_d3d.bat
@@ -0,0 +1,24 @@
+%w - Number of video columns%
+%h - Number of video rows%
+%n - Order of video cell in video wall, starting from 0%
+%m - Monitor identifier, integer start from 0%
+%t - 0 to disable, 1 to enable window¡¯s title%
+%tmo - Timeout, in seconds%
+
+start /b sample_decode.exe h264 -i  C:\Users\NUC\Downloads\svet_e2e_sample_w\svet_e2e_sample_w\video_e2e_sample\content\classroom.h264 -d3d -hw -wall 4 4 0 0 1 180
+start /b sample_decode.exe h264 -i  C:\Users\NUC\Downloads\svet_e2e_sample_w\svet_e2e_sample_w\video_e2e_sample\content\classroom.h264 -d3d -hw -wall 4 4 1 0 1 180
+start /b sample_decode.exe h264 -i  C:\Users\NUC\Downloads\svet_e2e_sample_w\svet_e2e_sample_w\video_e2e_sample\content\classroom.h264 -d3d -hw -wall 4 4 2 0 1 180
+start /b sample_decode.exe h264 -i  C:\Users\NUC\Downloads\svet_e2e_sample_w\svet_e2e_sample_w\video_e2e_sample\content\classroom.h264 -d3d -hw -wall 4 4 3 0 1 180
+start /b sample_decode.exe h264 -i  C:\Users\NUC\Downloads\svet_e2e_sample_w\svet_e2e_sample_w\video_e2e_sample\content\classroom.h264 -d3d -hw -wall 4 4 4 0 1 180
+start /b sample_decode.exe h264 -i  C:\Users\NUC\Downloads\svet_e2e_sample_w\svet_e2e_sample_w\video_e2e_sample\content\classroom.h264 -d3d -hw -wall 4 4 5 0 1 180
+start /b sample_decode.exe h264 -i  C:\Users\NUC\Downloads\svet_e2e_sample_w\svet_e2e_sample_w\video_e2e_sample\content\classroom.h264 -d3d -hw -wall 4 4 6 0 1 180
+start /b sample_decode.exe h264 -i  C:\Users\NUC\Downloads\svet_e2e_sample_w\svet_e2e_sample_w\video_e2e_sample\content\classroom.h264 -d3d -hw -wall 4 4 7 0 1 180
+                                               
+start /b sample_decode.exe h264 -i  C:\Users\NUC\Downloads\svet_e2e_sample_w\svet_e2e_sample_w\video_e2e_sample\content\classroom.h264 -d3d -hw -wall 4 4 8 0 1 180
+start /b sample_decode.exe h264 -i  C:\Users\NUC\Downloads\svet_e2e_sample_w\svet_e2e_sample_w\video_e2e_sample\content\classroom.h264 -d3d -hw -wall 4 4 9 0 1 180
+start /b sample_decode.exe h264 -i  C:\Users\NUC\Downloads\svet_e2e_sample_w\svet_e2e_sample_w\video_e2e_sample\content\classroom.h264 -d3d -hw -wall 4 4 10 0 1 180
+start /b sample_decode.exe h264 -i  C:\Users\NUC\Downloads\svet_e2e_sample_w\svet_e2e_sample_w\video_e2e_sample\content\classroom.h264 -d3d -hw -wall 4 4 11 0 1 180
+start /b sample_decode.exe h264 -i  C:\Users\NUC\Downloads\svet_e2e_sample_w\svet_e2e_sample_w\video_e2e_sample\content\classroom.h264 -d3d -hw -wall 4 4 12 0 1 180
+start /b sample_decode.exe h264 -i  C:\Users\NUC\Downloads\svet_e2e_sample_w\svet_e2e_sample_w\video_e2e_sample\content\classroom.h264 -d3d -hw -wall 4 4 13 0 1 180
+start /b sample_decode.exe h264 -i  C:\Users\NUC\Downloads\svet_e2e_sample_w\svet_e2e_sample_w\video_e2e_sample\content\classroom.h264 -d3d -hw -wall 4 4 14 0 1 180
+start /b sample_decode.exe h264 -i  C:\Users\NUC\Downloads\svet_e2e_sample_w\svet_e2e_sample_w\video_e2e_sample\content\classroom.h264 -d3d -hw -wall 4 4 15 0 1 180
---
+++ h264_dec_display_16ch_d3d11.bat
@@ -0,0 +1,24 @@
+%w - Number of video columns%
+%h - Number of video rows%
+%n - Order of video cell in video wall, starting from 0%
+%m - Monitor identifier, integer start from 0%
+%t - 0 to disable, 1 to enable window¡¯s title%
+%tmo - Timeout, in seconds%
+
+start /b sample_decode.exe h264 -i  ../Samples/blue_sky_1080p_libx264.h264 -d3d11 -hw -wall 4 4 0 0 1 180
+start /b sample_decode.exe h264 -i  ../Samples/blue_sky_1080p_libx264.h264 -d3d11 -hw -wall 4 4 1 0 1 180
+start /b sample_decode.exe h264 -i  ../Samples/blue_sky_1080p_libx264.h264 -d3d11 -hw -wall 4 4 2 0 1 180
+start /b sample_decode.exe h264 -i  ../Samples/blue_sky_1080p_libx264.h264 -d3d11 -hw -wall 4 4 3 0 1 180
+start /b sample_decode.exe h264 -i  ../Samples/blue_sky_1080p_libx264.h264 -d3d11 -hw -wall 4 4 4 0 1 180
+start /b sample_decode.exe h264 -i  ../Samples/blue_sky_1080p_libx264.h264 -d3d11 -hw -wall 4 4 5 0 1 180
+start /b sample_decode.exe h264 -i  ../Samples/blue_sky_1080p_libx264.h264 -d3d11 -hw -wall 4 4 6 0 1 180
+start /b sample_decode.exe h264 -i  ../Samples/blue_sky_1080p_libx264.h264 -d3d11 -hw -wall 4 4 7 0 1 180
+                                               
+start /b sample_decode.exe h264 -i  ../Samples/blue_sky_1080p_libx264.h264 -d3d11 -hw -wall 4 4 8 0 1 180
+start /b sample_decode.exe h264 -i  ../Samples/blue_sky_1080p_libx264.h264 -d3d11 -hw -wall 4 4 9 0 1 180
+start /b sample_decode.exe h264 -i  ../Samples/blue_sky_1080p_libx264.h264 -d3d11 -hw -wall 4 4 10 0 1 180
+start /b sample_decode.exe h264 -i  ../Samples/blue_sky_1080p_libx264.h264 -d3d11 -hw -wall 4 4 11 0 1 180
+start /b sample_decode.exe h264 -i  ../Samples/blue_sky_1080p_libx264.h264 -d3d11 -hw -wall 4 4 12 0 1 180
+start /b sample_decode.exe h264 -i  ../Samples/blue_sky_1080p_libx264.h264 -d3d11 -hw -wall 4 4 13 0 1 180
+start /b sample_decode.exe h264 -i  ../Samples/blue_sky_1080p_libx264.h264 -d3d11 -hw -wall 4 4 14 0 1 180
+start /b sample_decode.exe h264 -i  ../Samples/blue_sky_1080p_libx264.h264 -d3d11 -hw -wall 4 4 15 0 1 180
